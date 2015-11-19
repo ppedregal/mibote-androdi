ebbea610f3b9ab4792e574c29d8b70ab1eddddd4
Pablo Pedregal <ppedregal@vectorsf.com>
2015-11-19 23:41:46 +0100

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/ppedregal/mibote-android.git
    
    Conflicts:
    	Botin/bin/Botin.apk
    	Botin/bin/classes.dex
    	Botin/bin/classes/com/thinknowa/botin/BttMainActivity.class
    	Botin/bin/classes/com/thinknowa/botin/BuildConfig.class
    	Botin/bin/classes/com/thinknowa/botin/R$attr.class
    	Botin/bin/classes/com/thinknowa/botin/R$id.class
    	Botin/bin/classes/com/thinknowa/botin/R$layout.class
    	Botin/bin/classes/com/thinknowa/botin/R$menu.class
    	Botin/bin/classes/com/thinknowa/botin/R$string.class
    	Botin/bin/classes/com/thinknowa/botin/R$style.class
    	Botin/bin/classes/com/thinknowa/botin/R.class
    	Botin/bin/classes/com/thinknowa/botin/components/slidemenu/PagerAdapter.class
    	Botin/bin/classes/com/thinknowa/botin/components/slideritem/model/Track.class
    	Botin/bin/classes/com/thinknowa/botin/components/slideritem/view/AlbumArt.class
    	Botin/bin/classes/com/thinknowa/botin/components/slideritem/view/AlbumArtView$1.class
    	Botin/bin/classes/com/thinknowa/botin/components/slideritem/view/AlbumArtView.class
    	Botin/bin/classes/com/thinknowa/botin/components/slideritem/view/FileChooser.class
    	Botin/bin/classes/com/thinknowa/botin/data/ItemManager.class
    	Botin/bin/resources.ap_

--- sdk/sdk/src/main/java/com/thinknowa/botin/sdk/TinApi.java
+++ sdk/sdk/src/main/java/com/thinknowa/botin/sdk/TinApi.java
@@ -1,9 +1,15 @@
 package com.thinknowa.botin.sdk;
 
+import java.util.List;
+
+import com.thinknowa.botin.sdk.model.Account;
+import com.thinknowa.botin.sdk.model.Movement;
 import com.thinknowa.botin.sdk.model.Tin;
 
 import retrofit.http.GET;
+import retrofit.http.POST;
 import retrofit.http.Path;
+import retrofit.http.Query;
 
 /**
  * Created by ppedregal on 18/11/15.
@@ -12,5 +18,23 @@ public interface TinApi {
 
     @GET("tins/{id}")
     Tin get(@Path("id") String id);
+    
+    @POST("tins/{id}/disolve")
+    Void disolve(@Path("id") String id);
 
+    @POST("tins/{id}/invite")
+    Void invite(@Path("id") String id,@Query("accountId")String accountId);
+    
+    @POST("tins/{id}/join")
+    Void join(@Path("id") String id,@Query("accountId")String accountId);
+    
+    @POST("tins/{id}/leave")
+    Void leave(@Path("id") String id,@Query("accountId")String accountId);
+    
+    @GET("tins/{id}/members")
+    List<Account> getMembers(@Path("id") String id);
+    
+    @GET("tins/{id}/movements")
+    List<Movement> getMovements(@Path("id")String id);
+    
 }
--- sdk/sdk/src/main/java/com/thinknowa/botin/sdk/model/Tin.java
+++ sdk/sdk/src/main/java/com/thinknowa/botin/sdk/model/Tin.java
@@ -7,6 +7,7 @@ public class Tin {
 
     private String id;
     private String name;
+    private String type;
     private Amount amount;
 
     public String getId() {
@@ -32,6 +33,14 @@ public class Tin {
     public void setAmount(Amount amount) {
         this.amount = amount;
     }
+    
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
 
 	@Override
 	public String toString() {
